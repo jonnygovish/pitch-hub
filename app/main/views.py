6ff30e2093abc2ddb80d633ca278db8bf6f3666b
John <jonnygovish@gmail.com>
2017-10-30 10:40:26 +0300

    Application folder structure

---
+++ app/main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+
+@main.route('/')
+def index():
+  """
+  """
+  title = "Welcome to Pitch Hub"
+  return render_template('index.html', title = title)
