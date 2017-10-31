ea1fe3b27f1d02bee0c2fa027d3a739473bb15de
John <jonnygovish@gmail.com>
2017-10-31 04:10:29 +0300

    Importing the migrate mdolue into manage.py and initalizing it

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 from app import create_app,db 
 from flask_script import Manager,Server
-
+from app.models import Pitch,Category
+from flask_migrate import Migrate,MigrateCommand
 #creating app instance
 app = create_app('development')
 
+migrate = Migrate(app,db)
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 manager.add_command('server',Server)
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db =db)
+    return dict(app = app,db =db,Pitch= Pitch, Category = Category)
 if __name__=='__main__':
   manager.run()
