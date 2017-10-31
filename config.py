b8ff8e875d14e32016cff0fec104e8d3803da073
John <jonnygovish@gmail.com>
2017-10-31 04:08:30 +0300

    updating config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
   SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://puppah:zanpakutou@localhost/pitch_hub'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY=os.environ.get('SECRET_KEY')
 class ProdConfig(Config):
   pass
 
