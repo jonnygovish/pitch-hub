0d5e164181580c0aa88a1f5c849232f57377c1ac
John <jonnygovish@gmail.com>
2017-11-01 04:55:14 +0300

    Create a Comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,8 @@ from wtforms.validators import Required
 class PitchForm(FlaskForm):
   content = TextAreaField('New Pitch')
   submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+  body = TextAreaField('New Comment')
+  submit = SubmitField('Submit')
